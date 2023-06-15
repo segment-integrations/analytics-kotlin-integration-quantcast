685ea262b58c5fdff5aca5ab1ab72a480965e98c
Michael Grosse Huelsewiesche <mihuelsewiesche@twilio.com>
2023-06-15 18:20:46 -0400

    Preparing to rename repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "analytics-kotlin-integration-quantcast"
+rootProject.name = "analytics-kotlin-quantcast"
 include(":lib")
 includeBuild("publishing-plugins")
 include(":testapp")
